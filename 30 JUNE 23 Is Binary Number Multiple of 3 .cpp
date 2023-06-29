a6fcc755c5a27b7c2c236d90ae387c718f9aac65
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-06-30 01:09:20 +0530

    Update 30 JUNE 23 Is Binary Number Multiple of 3 .cpp

--- 30 JUNE 23 Is Binary Number Multiple of 3 .cpp
+++ 30 JUNE 23 Is Binary Number Multiple of 3 .cpp
@@ -1,8 +1,17 @@
 https://practice.geeksforgeeks.org/problems/is-binary-number-multiple-of-30654/1
 
-int isDivisible(string s){
+	int isDivisible(string s){
 	   //   If the difference between the count of odd set bits (Bits set at odd positions) and even set bits is a multiple of 3 then the number is divisible by 3.
-	      
+	   //  2^4 * a + 2^3 * b + 2^2 * c + 2^1 * d + 2^0 * e
+	   //every even power of 2 can be represented as 3n + 1, and every odd power of 2 can be represented as 3n - 1
+	   // 2^0 = 3 * 0 + 1
+           //  2^1 = 3 * 1 - 1
+           //  2^2 = 3 * 1 + 1
+           //  2^3 = 3 * 3 - 1
+	   //  (3n + 1) * a + (3n - 1) * b + (3n + 1) * c + (3n - 1) * d + (3n + 1) * e
+	   //  (3n)(a + b + c + d + e) + (a - b + c - d + e)
+	   //  To have this number divisible by 3, the term (a + c + e) - (b + d) should be divisible by 3. 
+	   
 	   int odd=0, even=0;
 	   reverse(s.begin(), s.end());
 	   
