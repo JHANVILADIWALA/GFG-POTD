9bd583239eeec68c72fbdacdc57cd80a13a48b99
Jhanvi Ladiwala <jhanviladiwalapf@gmail.com>
2023-07-03 12:59:40 +0530

    Create 03 JUNE 23 Maximum Index .cpp

---
+++ 03 JUNE 23 Maximum Index .cpp
@@ -0,0 +1,27 @@
+https://practice.geeksforgeeks.org/problems/maximum-index3307/1
+
+int maxIndexDiff(int arr[], int n) {
+       vector<int> prefix(n), suffix(n);
+        prefix[0] = arr[0];
+        suffix[n - 1] = arr[n - 1];
+        
+        for(int i = 1; i < n; i++){
+            prefix[i] = min(prefix[i - 1], arr[i]);
+            suffix[n - i - 1] = max(suffix[n - i], arr[n - i - 1]);
+        }
+        
+        int i, j, ans;
+        i = j = ans = 0;
+        
+        while(j < n){
+            if(prefix[i] <= suffix[j]){
+                ans = max(ans, j - i);
+                ++j;
+            }
+            else{
+                ++i;
+            }
+        }
+        
+        return ans;
+    }
